bb4da0fd87b597de44a7641f2fdba6c539900fc6
Delache <delache33@gmail.com>
2019-12-02 17:10:09 +0100

    ReactiveForm16

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,7 +1,9 @@
 import { User } from './../shared/models/user';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { passwordValidator } from './passwordValidator';
+import { passwordValidator } from './password-validator';
+import { emailValidator } from './email-validator';
+
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
@@ -13,7 +15,7 @@ export class SignInComponent implements OnInit {
   userForm = this.fb.group({
     username: ['', [Validators.required, Validators.minLength(4)]],
     credentials: this.fb.group({
-      email: [''],
+      email: ['', [Validators.required, emailValidator]],
       password: ['', [Validators.required, passwordValidator]],
     }),
     addressForm: this.fb.group({
